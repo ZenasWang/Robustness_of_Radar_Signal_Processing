9fa6ee77543820342064db9fa4fa67b63293b554
st164369 <st164369@stud.uni-stuttgart.de>
2019-10-31 19:31:01 +0100

    generate the signal model

---
+++ Generator.m
@@ -0,0 +1,27 @@
+
+tpn = 0 : T_pn: T_full - T_full / N_pn;
+k = 0 : N_chirp - 1; % in which chirp
+ns = 0 : N_sample - 1;   % in which sample
+
+fD = -2 * f0 * vr / c0;     % 1 x N_pn
+fR = -2 * ramp * r0 / c0;   %scalar
+
+for i = 1 : N_pn
+    X(:, :, i) = A * exp(1j * 2 * pi * (-2 * f0(i) * r0 / c0))...
+      * exp(1j * 2 * pi * (fD(i) * tpn(i)))...           % scalar
+      * exp(1j * 2 * pi * fR * ns' * T_sample)...        % N_sample x 1
+      * exp(1j * 2 * pi * fD(i) * k * T_chirp)...        % 1 x N_T_chirp
+      * exp(1j * 2 * pi * f0(i) / c0 * (P_Tx(i) + P_Rx(i)) * u);
+end
+
+% mu = zeros(ns, k, N_pn);
+% sigma = sigma0 * eye();
+sigma = 100000*eye(1000);
+mu = zeros(1000, 1000);
+rng('default')  % For reproducibility
+R = mvnrnd(mu,sigma);
+scatter3(R(:,1),R(:,2),R(:,3))
+
+% subplot(2,1,1); stem(X(:, 1, 2));
+% fftx = fft(X(:, 1, 2));
+% subplot(2,1,2); stem(fftx);
---
+++ InputParameters.m
@@ -0,0 +1,26 @@
+clear all
+%define the configration module parameters
+N_pn = 8;       % pulse number
+N_chirp = 1000;  % chirp number per pulse
+N_sample = 20;  % sample number per chirp
+
+T_full = 8e-3;                     % full time
+T_pn = T_full / N_pn;            % duration per pulse
+T_chirp = T_pn / N_chirp;        % chirp duration
+T_sample = T_chirp / N_sample;   % sample duration
+
+B = 1e9;                        %bandwidth
+ramp = B / T_chirp;             % chirp rate
+f0(1: N_pn) = 70e9;              % carrier frequency
+
+% antenna_configuration
+P_Tx(1: N_pn) = 0;
+P_Rx(1: N_pn) = 10;
+A = 10; 
+c0 = 3e8;
+
+%define the scenatio modeule parameters
+r0 = 100;
+vr = 0;
+sigma0 = 6;
+u = pi / 180 * 15;

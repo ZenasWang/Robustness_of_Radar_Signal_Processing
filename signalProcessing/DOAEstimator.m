13bff9ecd9634c02ebc0efa1180546885cfc9cc1
st164369 <st164369@stud.uni-stuttgart.de>
2019-12-04 13:34:45 +0100

    interpolation for doa estimation

--- signalProcessing/DOAEstimator.m
+++ signalProcessing/DOAEstimator.m
@@ -2,48 +2,40 @@ function [ angle ] = DOAEstimator( arrayResponse,radarParameter, r0_hat, vr_hat)
 %BARTLETT_ANGLEESTI: Calculate the Bartlett Spectrum as the Correlation of
 %the phase shifted channels outputs and estimate the DOA by Maximizing the
 %Bartlett Spectrum
-% - arrayResponse   := signal after pulse compression
-% - radarParameters := The defines Radar Modulation Parameters
+% - arrayResponse    := signal after pulse compression
+% - radarParameters  := The defines Radar Modulation Parameters
 % - angle            := The estimated DOA
 %      
 %
 % angle estimation
-% az = -2*pi : (pi/100): pi*2;
-% el = -pi*2 : (pi/100): pi*2;
-ux = -1 : 0.00001 : 1;
-uy = 0;
+ux = -1 : 0.01 : 1;
+uy = 0 : 0.01 : 1;
 tpn = (0 : radarParameter.N_Tx - 1) * radarParameter.T_pn;
 E = -2*pi /radarParameter.c0 * [2 * kron(radarParameter.f0', ones([radarParameter.N_Rx,1])),...
                                + 2 * kron(radarParameter.f0' .* tpn', ones([radarParameter.N_Rx,1])),...
                                + kron(radarParameter.f0', ones([radarParameter.N_Rx,1])) .* radarParameter.P];
-
 for x = 1 : length(ux)%(az)
   for y = 1 : length(uy)%(el)
     % ideal angle vector
-%     u_ideal = [sin(az(azcount))*cos(el(elcount)); sin(el(elcount)); cos(el(elcount))*cos(az(azcount))];
     u_ideal = [ux(x); uy(y); sqrt(1 - ux(x)^2 + uy(y) ^2)];
-% ideal Signal
+    % ideal Signal
     X_ideal = exp(1j * E * [r0_hat; vr_hat; u_ideal]);
     % Bartlett Spectrum
-%     B(azcount,elcount) = ((abs(arrayResponse' * X_ideal)).^2);
     B(x,y) = abs(arrayResponse' * X_ideal).^2;
-%     L(x,y) = abs(exp(1j*(E * [r0_hat; vr_hat;0;0;0]))' * exp(1j*(E * [r0_hat; vr_hat; u_ideal]))).^2;
   end
 end
 [ux_ind,uy_ind] = find(B == max(B(:)));
-% hold on
-% lamda = radarParameter.c0 / radarParameter.f0(1);
-% % distance between each antenna for 均匀分布
-% d = lamda/2 * 2;
-% u_3db = 0.891 * lamda/2/(radarParameter.N_pn)/d;
-% % X_ideal_0 = exp(1j * E * [r0_hat; vr_hat; ux(ux_ind) + u_3db/2; uy(uy_ind) + u_3db/2;...
-% %     sqrt(1 - (ux(ux_ind) + u_3db/2)^2 - (ux(ux_ind) + u_3db)/2)]);
-% % % plot3(ux(ux_ind) + u_3db/2, uy(uy_ind) + u_3db/2, abs(arrayResponse' * X_ideal_0).^2, 'o')
-% a = abs(sum(exp(1j*(E * [r0_hat; vr_hat;...
-%     ux(ux_ind(1)) + u_3db/2; uy(uy_ind(1)) + u_3db/2;...
-%     sqrt(1 - (ux(ux_ind(1)) + u_3db/2)^2 - (uy(uy_ind(1)) + u_3db/2)^2)])))).^2
-% b = L(ux_ind(1),uy_ind(1))/2
-% angle = [az(az_ind)/pi*180, el(el_ind)/pi*180];
-angle = [ux(ux_ind(1)), uy(uy_ind(1))];
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 这里要做插值 inter2
+ux_fine = ux(ux_ind-3) : 0.00001 : ux(ux_ind+3);
+uy_fine = uy(uy_ind-3) : 0.00001 : uy(uy_ind+3);
+[X, Y] = meshgrid((ux(ux_ind-3):0.01:ux(ux_ind+3))', uy(uy_ind-3):0.01:uy(uy_ind+3));
+[Xq, Yq]= meshgrid(ux_fine', uy_fine);
+V = B((ux_ind-3:ux_ind+3), (uy_ind-3:uy_ind+3));
+Vb = interp2(X,Y,V,Xq,Yq,'cubic');
+[ux_fine_ind,uy_fine_ind] = find(Vb == max(Vb(:)));
+% surf(Vb)
+% angle = [ux(ux_ind(1)), uy(uy_ind(1))];
+angle = [ux_fine(ux_fine_ind), uy_fine(uy_fine_ind)];
 end
 
